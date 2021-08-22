50455c26e4cdd41263344e9b27990449c20056f7
Bruno Westphal <b.westphal@biel-net.de>
2021-08-22 21:20:21 +0200

    init commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"snmp_bw adapter settings": {
+		"en": "Adapter settings for snmp_bw",
+		"de": "Adaptereinstellungen für snmp_bw",
+		"ru": "Настройки адаптера для snmp_bw",
+		"pt": "Configurações do adaptador para snmp_bw",
+		"nl": "Adapterinstellingen voor snmp_bw",
+		"fr": "Paramètres d'adaptateur pour snmp_bw",
+		"it": "Impostazioni dell'adattatore per snmp_bw",
+		"es": "Ajustes del adaptador para snmp_bw",
+		"pl": "Ustawienia adaptera dla snmp_bw",
+		"zh-cn": "snmp_bw的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项 1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
